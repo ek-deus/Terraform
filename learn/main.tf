4debfcecd39023120e759e32e17c8d5cbf690e75
E.K <92515284+ek-deus@users.noreply.github.com>
2023-06-08 23:45:03 +0300

    main.tf: add provider

--- learn/main.tf
+++ learn/main.tf
@@ -1,1 +1,20 @@
+# создание провайдера
+
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "<зона доступности по умолчанию>"
+}
+
+# source — глобальный адрес источника провайдера.
+# required_version — минимальная версия Terraform, с которой совместим провайдер.
+# provider — название провайдера.
+# zone — зона доступности, в которой по умолчанию будут создаваться все облачные ресурсы.
 
